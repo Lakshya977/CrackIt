636fe85dd5f67c7ff29daac4a2ba11ad930c841e
Lakshya <Lakshya8998@gmail.com>
2025-05-23 02:02:59 +0530

    initialize project with Next.js, Tailwind CSS, and necessary dependencies

---
+++ app/providers.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { ThemeProviderProps } from 'next-themes'
+import {HeroUIProvider} from '@heroui/react'
+
+
+interface ProvidersProps {
+    children: React.ReactNode;
+    themeProps?: ThemeProviderProps;  
+}
+
+export function Providers({children,themeProps}:ProvidersProps) {
+  return (
+    <HeroUIProvider>
+    
+      {children}
+    
+    </HeroUIProvider>
+  )
+}
---
+++ config/logo.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { Icon } from "@iconify/react";
+
+export const Logo = ({ size = 34 }) => (
+  <Icon icon="hugeicons:ai-brain-01" fontSize={size} />
+);
+//
